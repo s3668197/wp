3906d1eed0ab19a70174d6e6a91b113a631e7b07
s3668197 <53253433+s3668197@users.noreply.github.com>
2019-10-16 00:16:14 +1100

    Add files via upload
    In this first tools.php commit, an if statement was added to the code also a line which appends it to bookings.txt using the "fputcsv()" function

--- a4/tools.php
+++ a4/tools.php
@@ -1,6 +1,16 @@
 <?php
-  session_start();
+if ( empty( $_SESSION['cust'] || $_SESSION['movie'] || $_SESSION['seats'] )
+header('Location: index.php');
+?>  
 
-// Put your PHP functions and modules here
-
+<?php
+$now = date('d/m h:i');
+$total = // calculate the total serverside and format it to 2 decimal places
+$cells = array_merge( 
+  [ $now ], 
+  $_SESSION['cust'],
+  $_SESSION['movie'],
+  $_SESSION['seats'],
+  (array) $total 
+);
 ?>

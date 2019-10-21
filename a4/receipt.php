4106d9a298ee85fbf3de54a32c2932dc971af298
s3668197 <53253433+s3668197@users.noreply.github.com>
2019-10-22 00:24:56 +1100

    Receipt - Customer Details
    This receipt commit includes code which will get customer details from the session, along with the purchase summary.

--- a4/receipt.php
+++ a4/receipt.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "Receipt">
 
 
@@ -9,6 +10,7 @@
 	<link rel='stylesheet' type='text/css' href='css/style.css' />
 	
 	
+
 </head>
 
 <body>
@@ -41,7 +43,8 @@ Phone: (+61) 123-456-789</textarea>
 		
 		<div id="customer">
 
-            <textarea id="customer-title">Lunardo Cinema - Purchase Summary</textarea>
+            <textarea id="customer-title">Lunardo Cinema  Purchase Summary</textarea>
+           
 
             <table id="meta">
                 <tr>
@@ -54,14 +57,84 @@ Phone: (+61) 123-456-789</textarea>
                     <td><textarea id="date">December 15, 2009</textarea></td>
                 </tr>
                 <tr>
-                    <td class="meta-head">Amount Due</td>
-                    <td><div class="due">$875.00</div></td>
+                    <td class="meta-head">ABN</td>
+                    <td><div class="due"> 12 345 678 9100</div></td>
                 </tr>
 
             </table>
 		
-		</div>
-		
+    </div>
+    
+    <div>
+    <h2>CUSTOMER DETAILS</h2><br>   
+    <p>Name: <?php echo $receivedname; ?> </p>
+    <p>Email: <?php echo $receivedemail; ?> </p>  
+    <p>Mobile: <?php echo $receivedmobile; ?> </p><br><br><br>
+</div>
+
+<table style="width:90%">
+  <tr>
+    <th>Seat</th>
+    <th>Quantity</th> 
+    <th>Unit Price</th>
+    <th>Total Price</th>
+  </tr>
+  <tr>
+    <td>Standard Adult</td>
+    <td><?php echo $staQuantity; ?></td>
+    <td>$14.00</td>
+    <td>$<?php echo $totalForStandardAdult; ?></td>
+  </tr>
+  <tr>
+    <td>Standard Concession</td>
+    <td><?php echo $stpQuantity; ?></td>
+    <td>$12.50</td>
+    <td>$<?php echo $totalForStandardConcession; ?></td>
+  </tr>
+  <tr>
+    <td>Standard Child</td>
+    <td><?php echo $stcQuantity; ?></td>
+    <td>$11.00</td>
+    <td>$<?php echo $totalForStandardChild; ?></td>
+  </tr>
+  <tr>
+    <td>First Class Adult</td>
+    <td><?php echo $fcaQuantity; ?></td>
+    <td>$24.00</td>
+    <td>$<?php echo $totalForFirstClassAdult; ?></td>
+  </tr>
+  <tr>
+    <td>First Class Concession</td>
+    <td><?php echo $fcpQuantity; ?></td>
+    <td>$22.50</td>
+    <td>$<?php echo $totalForFirstClassConcession; ?></td>
+  </tr>
+   <tr>
+    <td>First Class Child</td>
+    <td><?php echo $fccQuantity; ?></td>
+    <td>$21.00</td>
+    <td>$<?php echo $totalForFirstClassChild; ?></td>
+  </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>SUBTOTAL:</td>
+    <td>$<?php echo $subtotal; ?></td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>GST:</td>
+    <td>$<?php echo $GST; ?></td>
+    </tr>
+    <tr>
+    <td></td>
+    <td></td>
+    <td>TOTAL:</td>
+    <td>$<?php echo $TOTALPRICE; ?></td>
+    </tr>
+  
+</table>
 		
 
 		<div id="terms">

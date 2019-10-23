e2922414da4ae202f6af81175bd279e62eae0d9a
s3707846 <37385526+s3707846@users.noreply.github.com>
2019-10-24 00:10:00 +1100

    Add files via upload
    Final version of receipt page.

--- a4/receipt.php
+++ a4/receipt.php
@@ -7,29 +7,22 @@ if (empty($_SESSION)) {
     header('Location: https://titan.csit.rmit.edu.au/~s3707846/wp/a4/index.php');
 }
 
-
 $receivedname = $_SESSION["cust"]["name"];
 $receivedemail = $_SESSION["cust"]["email"];
 $receivedmobile = $_SESSION["cust"]["mobile"];
-
 $staQuantity = $_SESSION["seats"]["STA"];
 $stpQuantity = $_SESSION["seats"]["STP"];
 $stcQuantity = $_SESSION["seats"]["STC"];
 $fcaQuantity = $_SESSION["seats"]["FCA"];
 $fcpQuantity = $_SESSION["seats"]["FCP"];
 $fccQuantity = $_SESSION["seats"]["FCC"];
-
 $totalForStandardAdult = number_format((float)$staQuantity*14.00, 2, '.', '');
 $totalForStandardConcession = number_format((float)$stpQuantity*12.50, 2, '.', '');
 $totalForStandardChild = number_format((float)$stcQuantity*11.00, 2, '.', '');
-
 $totalForFirstClassAdult = number_format((float)$fcaQuantity*24.00, 2, '.', '');
 $totalForFirstClassConcession = number_format((float)$fcpQuantity*22.50, 2, '.', '');
 $totalForFirstClassChild = number_format((float)$fccQuantity*21.00, 2, '.', '');
-
 $subtotal = number_format((float)$totalForStandardAdult + $totalForStandardConcession + $totalForStandardChild + $totalForFirstClassAdult + $totalForFirstClassConcession + $totalForFirstClassChild, 2, '.', '');
-
-
  $GST = number_format((float)$subtotal/11, 2, '.', '');
  $TOTALPRICE = number_format((float)$subtotal+$GST, 2, '.', '');
  ?>
@@ -37,24 +30,24 @@ $subtotal = number_format((float)$totalForStandardAdult + $totalForStandardConce
 
 
 <head>
-	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-	
-	<title>Receipt</title>
-	
-	<link rel='stylesheet' type='text/css' href='receiptstyle.css' />
-	
-	
+    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+
+    <title>Receipt</title>
+
+    <link rel='stylesheet' type='text/css' href='receiptstyle.css' />
+
+
 
 </head>
 
 <body>
 
-	<div id="page-wrap">
+    <div id="page-wrap">
 
-		<textarea id="header">INVOICE</textarea>
-		
-		<div id="identity">
-		
+        <textarea id="header">INVOICE</textarea>
+
+        <div id="identity">
+
             <textarea id="address">Lunardo Cinema
 123 Swanston Street
 VIC, 3000
@@ -65,20 +58,20 @@ Phone: (+61) 123-456-789</textarea>
 
 
 
-              <div id="logohelp">
-                <input id="imageloc" type="text" size="50" value="" /><br />
-              </div>
-			  <img src='https://titan.csit.rmit.edu.au/~s3668197/media/logo.png' alt='cinema logo'  width='200' height='120'/>
+                <div id="logohelp">
+                    <input id="imageloc" type="text" size="50" value="" /><br />
+                </div>
+                <img src='https://titan.csit.rmit.edu.au/~s3668197/media/logo.png' alt='cinema logo' width='200' height='120' />
             </div>
-		
-		</div>
-		
-		<div style="clear:both"></div>
-		
-		<div id="customer">
+
+        </div>
+
+        <div style="clear:both"></div>
+
+        <div id="customer">
 
             <textarea id="customer-title">Lunardo Cinema  Purchase Summary</textarea>
-           
+
 
             <table id="meta">
                 <tr>
@@ -92,92 +85,166 @@ Phone: (+61) 123-456-789</textarea>
                 </tr>
                 <tr>
                     <td class="meta-head">ABN</td>
-                    <td><div class="due"> 12 345 678 9100</div></td>
+                    <td>
+                        <div class="due"> 12 345 678 9100</div>
+                    </td>
                 </tr>
 
             </table>
-		
+
+        </div>
+
+        <div>
+            <h2>CUSTOMER DETAILS</h2><br>
+            <p>Name: <?php echo $receivedname; ?> </p>
+            <p>Email: <?php echo $receivedemail; ?> </p>
+            <p>Mobile: <?php echo $receivedmobile; ?> </p><br><br><br>
+        </div>
+
+        <table style="width:90%">
+            <tr>
+                <th>Seat</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Total Price</th>
+            </tr>
+            <tr>
+                <td>Standard Adult</td>
+                <td><?php echo $staQuantity; ?></td>
+                <td>$14.00</td>
+                <td>$<?php echo $totalForStandardAdult; ?></td>
+            </tr>
+            <tr>
+                <td>Standard Concession</td>
+                <td><?php echo $stpQuantity; ?></td>
+                <td>$12.50</td>
+                <td>$<?php echo $totalForStandardConcession; ?></td>
+            </tr>
+            <tr>
+                <td>Standard Child</td>
+                <td><?php echo $stcQuantity; ?></td>
+                <td>$11.00</td>
+                <td>$<?php echo $totalForStandardChild; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Adult</td>
+                <td><?php echo $fcaQuantity; ?></td>
+                <td>$24.00</td>
+                <td>$<?php echo $totalForFirstClassAdult; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Concession</td>
+                <td><?php echo $fcpQuantity; ?></td>
+                <td>$22.50</td>
+                <td>$<?php echo $totalForFirstClassConcession; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Child</td>
+                <td><?php echo $fccQuantity; ?></td>
+                <td>$21.00</td>
+                <td>$<?php echo $totalForFirstClassChild; ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>SUBTOTAL:</td>
+                <td>$<?php echo $subtotal; ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>GST:</td>
+                <td>$<?php echo $GST; ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>TOTAL:</td>
+                <td>$<?php echo $TOTALPRICE; ?></td>
+            </tr>
+
+        </table>
+
+
+        <div id="terms">
+            <h5> Lunardo Terms</h5>
+            <textarea>Prices quoted on this Website are in Australian dollars and include GST where applicable.</textarea>
+        </div>
+
     </div>
-    
-    <div>
-    <h2>CUSTOMER DETAILS</h2><br>   
-    <p>Name: <?php echo $receivedname; ?> </p>
-    <p>Email: <?php echo $receivedemail; ?> </p>  
-    <p>Mobile: <?php echo $receivedmobile; ?> </p><br><br><br>
-</div>
 
-<table style="width:90%">
-  <tr>
-    <th>Seat</th>
-    <th>Quantity</th> 
-    <th>Unit Price</th>
-    <th>Total Price</th>
-  </tr>
-  <tr>
-    <td>Standard Adult</td>
-    <td><?php echo $staQuantity; ?></td>
-    <td>$14.00</td>
-    <td>$<?php echo $totalForStandardAdult; ?></td>
-  </tr>
-  <tr>
-    <td>Standard Concession</td>
-    <td><?php echo $stpQuantity; ?></td>
-    <td>$12.50</td>
-    <td>$<?php echo $totalForStandardConcession; ?></td>
-  </tr>
-  <tr>
-    <td>Standard Child</td>
-    <td><?php echo $stcQuantity; ?></td>
-    <td>$11.00</td>
-    <td>$<?php echo $totalForStandardChild; ?></td>
-  </tr>
-  <tr>
-    <td>First Class Adult</td>
-    <td><?php echo $fcaQuantity; ?></td>
-    <td>$24.00</td>
-    <td>$<?php echo $totalForFirstClassAdult; ?></td>
-  </tr>
-  <tr>
-    <td>First Class Concession</td>
-    <td><?php echo $fcpQuantity; ?></td>
-    <td>$22.50</td>
-    <td>$<?php echo $totalForFirstClassConcession; ?></td>
-  </tr>
-   <tr>
-    <td>First Class Child</td>
-    <td><?php echo $fccQuantity; ?></td>
-    <td>$21.00</td>
-    <td>$<?php echo $totalForFirstClassChild; ?></td>
-  </tr>
-    <tr>
-    <td></td>
-    <td></td>
-    <td>SUBTOTAL:</td>
-    <td>$<?php echo $subtotal; ?></td>
-    </tr>
-    <tr>
-    <td></td>
-    <td></td>
-    <td>GST:</td>
-    <td>$<?php echo $GST; ?></td>
-    </tr>
-    <tr>
-    <td></td>
-    <td></td>
-    <td>TOTAL:</td>
-    <td>$<?php echo $TOTALPRICE; ?></td>
-    </tr>
-  
-</table>
-		
-
-		<div id="terms">
-		  <h5> Lunardo Terms</h5>
-		  <textarea>Prices quoted on this Website are in Australian dollars and include GST where applicable.</textarea>
-		</div>
-	
-	</div>
-	
 </body>
+
+
+<div class="cardWrap">
+    <div class="card cardLeft">
+        <h1>Lunardo <span>Cinema</span></h1>
+
+        <table id = 1px solid red>
+            
+            <th>Seats</th>
+            <th>Quantity</th>
+
+
+
+            <tr>
+                <td>Standard Adult</td>
+                <td><?php echo $staQuantity; ?></td>
+            </tr>
+            <tr>
+                <td>Standard Concession</td>
+                <td><?php echo $stpQuantity; ?></td>
+            </tr>
+            <tr>
+                <td>Standard Child</td>
+                <td><?php echo $stcQuantity; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Adult</td>
+                <td><?php echo $fcaQuantity; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Concession</td>
+                <td><?php echo $fcpQuantity; ?></td>
+            </tr>
+            <tr>
+                <td>First Class Child</td>
+                <td><?php echo $fccQuantity; ?></td>
+            </tr>
+
+        </table>
+
+
+
+
+
+
+        <div class="seat">
+            <h2>Seat</h2>
+            <span>B17</span>
+        </div>
+        <div class="screen">
+            <h2>Screen</h2>
+            <span>14</span>
+        </div>
+
+        <div class="screen">
+            <h2>Session</h2>
+            <span>12:30 - 3:30</span>
+        </div>
+
+
+    </div>
+    <div class="card cardRight">
+        <div class="eye"></div>
+        <div class="number">
+            <h3>TICKET</h3>
+            <span>seat</span>
+        </div>
+        <div class="barcode"></div>
+    </div>
+
+</div>
+    
 
 </html>
